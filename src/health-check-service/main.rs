e454e903c344effd85fe2ce642153499fe259329
Julien Gantner <dev@julien-gantner.de>
2025-03-10 07:13:25 +0100

    Implements health check service

--- src/health-check-service/main.rs
+++ src/health-check-service/main.rs
@@ -1,11 +1,11 @@
 use std::env;
 
+use crate::authentication::{SignInResponse, SignOutResponse, SignUpResponse, StatusCode};
 use authentication::auth_client::AuthClient;
 use authentication::{SignInRequest, SignOutRequest, SignUpRequest};
 use tokio::time::{sleep, Duration};
 use tonic::{Request, Response};
 use uuid::Uuid;
-use crate::authentication::{StatusCode, SignUpResponse, SignInResponse, SignOutResponse};
 pub mod authentication {
     tonic::include_proto!("authentication");
 }
@@ -17,29 +17,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Establish connection when auth service
     let mut client = AuthClient::connect(format!("http://{}:50051", auth_hostname)).await?;
     loop {
-        let username: String = todo!(); // Create random username using new_v4()
-        let password: String = todo!(); // Create random password using new_v4()
-        let request: Request<SignUpRequest> = todo!(); // Create a new `SignUpRequest`.
-        let response: Response<SignUpResponse> = todo!(); // Make a sign up request. Propagate any errors.
-        // Log the response
+        let username: String = Uuid::new_v4().to_string();
+        let password: String = Uuid::new_v4().to_string();
+        let request: Request<SignUpRequest> = Request::new(SignUpRequest { username: username.clone(), password: password.clone() });
+        let response: Response<SignUpResponse> = client.sign_up(request).await?;
+
         println!(
             "SIGN UP RESPONSE STATUS: {:?}",
             StatusCode::from_i32(response.into_inner().status_code)
         );
         // ---------------------------------------------
-        let request: Request<SignInRequest> = todo!(); // Create a new `SignInRequest`.
-        // Make a sign in request. Propagate any errors. Convert Response<SignInResponse> into SignInResponse.
-        let response: SignInResponse = todo!();
+        let request: Request<SignInRequest> = Request::new(SignInRequest { username: username, password}); 
+        let response: SignInResponse = client.sign_in(request).await?.into_inner();
         println!(
             "SIGN IN RESPONSE STATUS: {:?}",
-            todo!() // Log response status_code
+            StatusCode::from_i32(response.status_code)
         );
         // ---------------------------------------------
-        let request: Request<SignOutRequest> = todo!(); // Create a new `SignOutRequest`.
-        let response: Response<SignOutResponse> = todo!(); // Make a sign out request. Propagate any errors.
+        let request: Request<SignOutRequest> = Request::new(SignOutRequest { session_token: response.session_token });
+        let response: Response<SignOutResponse> = client.sign_out(request).await?; 
         println!(
             "SIGN OUT RESPONSE STATUS: {:?}",
-            todo!() // Log response status_code
+            StatusCode::from_i32(response.into_inner().status_code)
         );
         println!("--------------------------------------",);
         sleep(Duration::from_secs(3)).await;
