4aef6e8fae22de0cf0da9f66913fbc09c8bf322b
Julien Gantner <dev@julien-gantner.de>
2025-03-10 07:21:40 +0100

    removes redundant type information to clen up code

--- src/health-check-service/main.rs
+++ src/health-check-service/main.rs
@@ -1,10 +1,10 @@
 use std::env;
 
-use crate::authentication::{SignInResponse, SignOutResponse, SignUpResponse, StatusCode};
+use crate::authentication::StatusCode;
 use authentication::auth_client::AuthClient;
 use authentication::{SignInRequest, SignOutRequest, SignUpRequest};
 use tokio::time::{sleep, Duration};
-use tonic::{Request, Response};
+use tonic::Request;
 use uuid::Uuid;
 pub mod authentication {
     tonic::include_proto!("authentication");
@@ -17,25 +17,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Establish connection when auth service
     let mut client = AuthClient::connect(format!("http://{}:50051", auth_hostname)).await?;
     loop {
-        let username: String = Uuid::new_v4().to_string();
-        let password: String = Uuid::new_v4().to_string();
-        let request: Request<SignUpRequest> = Request::new(SignUpRequest { username: username.clone(), password: password.clone() });
-        let response: Response<SignUpResponse> = client.sign_up(request).await?;
+        let username = Uuid::new_v4().to_string();
+        let password = Uuid::new_v4().to_string();
+        let request = Request::new(SignUpRequest {
+            username: username.clone(),
+            password: password.clone(),
+        });
+        let response = client.sign_up(request).await?;
 
         println!(
             "SIGN UP RESPONSE STATUS: {:?}",
             StatusCode::from_i32(response.into_inner().status_code)
         );
         // ---------------------------------------------
-        let request: Request<SignInRequest> = Request::new(SignInRequest { username: username, password}); 
-        let response: SignInResponse = client.sign_in(request).await?.into_inner();
+        let request = Request::new(SignInRequest {
+            username: username,
+            password,
+        });
+        let response = client.sign_in(request).await?.into_inner();
         println!(
             "SIGN IN RESPONSE STATUS: {:?}",
             StatusCode::from_i32(response.status_code)
         );
         // ---------------------------------------------
-        let request: Request<SignOutRequest> = Request::new(SignOutRequest { session_token: response.session_token });
-        let response: Response<SignOutResponse> = client.sign_out(request).await?; 
+        let request = Request::new(SignOutRequest {
+            session_token: response.session_token,
+        });
+        let response = client.sign_out(request).await?;
         println!(
             "SIGN OUT RESPONSE STATUS: {:?}",
             StatusCode::from_i32(response.into_inner().status_code)
